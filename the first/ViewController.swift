d7f2cbd53e4d656cfad12979c8b7740f1a05803e
lajiezhuoma <lajiezhuoma@gmail.com>
2022-02-16 23:51:56 +1100

    initial ios app

---
+++ the first/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  the first
+//
+//  Created by 喇杰卓玛 on 2022/2/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
